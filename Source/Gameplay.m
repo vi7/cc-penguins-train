fa8e70e278b862973416a9c1803255d78207efd1
Vi <vi7alya@gmail.com>
2015-10-08 08:19:46 +0300

    Doing Physics

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -14,6 +14,9 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
+    CCNode *_mouseJointNode;
+    CCPhysicsJoint *_mouseJoint;
     
 }
 
@@ -21,15 +24,32 @@
     
     self.userInteractionEnabled = YES;
     
+    // visualize physics bodies & joints
+    //_physicsNode.debugDraw = YES;
+    
     CCNode *level = [CCBReader load:@"Levels/Level1"];
     [_levelNode addChild:level];
-    _physicsNode.debugDraw = YES;
+    
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
+    _mouseJointNode.physicsBody.collisionMask = @[];
     
 }
 
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     
-    [self launchPenguin];
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    // start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+    {
+        
+        _mouseJointNode.position = touchLocation;
+        
+        
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+        
+    }
     
 }
 
